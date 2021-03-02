c5562f1182bcb7bb520188e796d4be7b63560e4b
hwanil <uhi1117@naver.com>
2021-03-02 22:28:20 +0900

    11722

---
+++ boj/11722.cpp
@@ -0,0 +1,37 @@
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+int N, dp[1001], arr[1001];
+
+/*
+dp[i] : i번째를 마지막으로 하는 감소하는 부분수열 의 길이
+*/
+int main()
+{
+	scanf("%d", &N);
+	for (int i = 1; i <= N; ++i)
+	{
+		scanf("%d", &arr[i]);
+	}
+
+	for (int i = 1; i <= N; ++i)
+	{
+		dp[i] = 1;
+		for (int j = 1; j < i; ++j)
+		{
+			if (arr[i] < arr[j])
+			{
+				dp[i] = max(dp[j] + 1, dp[i]);
+			}
+		}
+	}
+
+	int result = 0;
+	for (int i = 1; i <= N; ++i) 
+	{
+		if (result < dp[i])
+			result = dp[i];
+	}
+	printf("%d\n", result);
+	return 0;
+}
